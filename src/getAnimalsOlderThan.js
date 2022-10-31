481b0f259b13c48bd3a3aa595e9a316115118b28
Vinicius José <viniciusjosedev@gmail.com>
2022-10-31 16:17:02 -0300

    requiito 2 finalizado

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const verific = data.species.find((elemento) => elemento.name === animal);
+  return verific.residents.every((elemento) => elemento.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
