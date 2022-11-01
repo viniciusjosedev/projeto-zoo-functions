0c664571c382672d8e5ee60ea2465e2b4a3a36aa
Vinicius José <viniciusjosedev@gmail.com>
2022-10-31 22:39:48 -0300

    requisito 4 finalizado

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,21 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.find((elemento) => elemento.managers.includes(id)) !== undefined;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const lista = [];
+  if (isManager(managerId)) {
+    data.employees.forEach((elemento) => {
+      if (elemento.managers.includes(managerId)) {
+        lista.push(`${elemento.firstName} ${elemento.lastName}`);
+      }
+    });
+    return lista;
+  } throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
+const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+console.log(getRelatedEmployees(stephanieId));
 
 module.exports = { isManager, getRelatedEmployees };
