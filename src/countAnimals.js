08699502fffb65aa280d908090ce785d96efa078
Vinicius José <viniciusjosedev@gmail.com>
2022-11-01 18:04:39 -0300

    requisito 5 finalizado

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,30 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function func(animal) {
+  const lista = [];
+  const verific = data.species.find((elemento) => elemento.name === animal.specie);
+  verific.residents.forEach((elemento, index) => {
+    if (elemento.sex === animal.sex) {
+      lista.push(elemento);
+    }
+  });
+  return lista.length;
 }
+
+function countAnimals(animal = '') {
+  if (animal.length === 0) {
+    const di = {};
+    const verific = data.species.map((elemento) => elemento.name);
+    verific.forEach((elemento) => {
+      di[elemento] = data.species.find((elemento2) => elemento2.name === elemento).residents.length;
+    });
+    return di;
+  } if (Object.keys(animal).length === 1) {
+    const verific = data.species.find((elemento) => elemento.name === animal.specie);
+    return verific.residents.length;
+  } return func(animal);
+}
+
+console.log(countAnimals({ specie: 'bears', sex: 'female' }));
 
 module.exports = countAnimals;
