0d6e7715a03fba7a6a5ae9160ca45113952877ff
Vinicius José <viniciusjosedev@gmail.com>
2022-11-02 09:40:44 -0300

    requisito 8 finalizado

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const dict = { adult: 0, child: 0, senior: 0 };
+  entrants.forEach((elemento) => {
+    if (elemento.age < 18) {
+      dict.child += 1;
+    } if (elemento.age >= 18 && elemento.age < 50) {
+      dict.adult += 1;
+    } if (elemento.age >= 50) {
+      dict.senior += 1;
+    }
+  });
+  return dict;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (typeof entrants === 'object' && Object.keys(entrants).length > 0) {
+    const dict = [countEntrants(entrants)];
+    return dict.reduce((acc, cur) => acc + (cur.adult * data.prices.adult)
+  + (cur.child * data.prices.child) + (cur.senior * data.prices.senior), 0);
+  } return 0;
 }
 
 module.exports = { calculateEntry, countEntrants };
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -3,30 +3,23 @@ const handlerElephants = require('../src/handlerElephants');
 describe('Testes da função HandlerElephants', () => {
   it('Verifica se é uma função', () => {
     expect('function').toEqual(typeof handlerElephants);
+  });
+  it('Verifica se retorna null quando o valor não é passado', () => {
     expect(handlerElephants()).toEqual(undefined);
+  });
+  it('Verifica se emite uma mensagem sobre o valor passado ser diferente de uma string', () => {
     expect(handlerElephants(12)).toEqual('Parâmetro inválido, é necessário uma string');
+  });
+  it('Verifica se o valor passado é uma chave existente no objeto', () => {
     expect(handlerElephants('name')).toEqual('elephants');
+  });
+  it('Verifica quantos elefantes existentes há no zoológico', () => {
     expect(handlerElephants('count')).toEqual(4);
+  });
+  it('Verifica se a lista de nomes de elefantes estão certas quando passado como valor a string nomes', () => {
     expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  });
+  it('Verifica se retorna null quando o valor passado não existe', () => {
     expect(handlerElephants('a')).toEqual(null);
   });
-  // });
-  // it('Verifica se retorna null quando o valor não é passado', () => {
-  //   // expect(handlerElephants()).toEqual(undefined);
-  // });
-  // it('Verifica se emite uma mensagem sobre o valor passado ser diferente de uma string', () => {
-  //   // expect(handlerElephants(12)).toEqual('Parâmetro inválido, é necessário uma string');
-  // });
-  // it('Verifica se o valor passado é uma chave existente no objeto', () => {
-  //   // expect(handlerElephants('name')).toEqual('elephants');
-  // });
-  // it('Verifica quantos elefantes existentes há no zoológico', () => {
-  //   expect(handlerElephants('count')).toEqual(4);
-  // });
-  // it('Verifica se a lista de nomes de elefantes estão certas quando passado como valor a string nomes', () => {
-  //   expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
-  // });
-  // it('Verifica se retorna null quando o valor passado não existe', () => {
-  //   expect(handlerElephants('a')).toEqual(null);
-  // });
 });
