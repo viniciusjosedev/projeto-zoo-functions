502c250f8750284a36f39488ee7093b2e3501e07
Vinicius José <viniciusjosedev@gmail.com>
2022-11-02 14:11:27 -0300

    requisito 10 finalizado

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const find = data.species.find((elemento) => elemento.id === data.employees
+    .find((elemento2) => elemento2.id === id).responsibleFor[0]).residents;
+  const verificacao = find.map((elemento) => elemento.age).sort((a, b) => b - a)[0];
+  return Object.values(find.find((elemento) => elemento.age === verificacao));
 }
+
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getOldestFromFirstSpecies;
