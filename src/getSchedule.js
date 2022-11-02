864235559aea9c60e7b2c54a1ccfad1f2d3bea76
Vinicius José <viniciusjosedev@gmail.com>
2022-11-02 13:29:40 -0300

    requisito 9 finalizado

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,44 @@
 const data = require('../data/zoo_data');
 
+const days = ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday'];
+
+function verific(nome) {
+  return data.species.find((elemento) => elemento.name === nome);
+}
+
+function animationSolo(nome) {
+  const dict = verific(nome);
+  return dict.availability;
+}
+
+function daySolo(nome) {
+  if (nome !== 'Monday') {
+    const animais = [];
+    data.species.forEach((elemento) => {
+      if (elemento.availability.includes(nome)) {
+        animais.push(elemento.name);
+      }
+    });
+    const nomes = `Open from ${data.hours[nome].open}am until ${data.hours[nome].close}pm`;
+    return { [nome]: { officeHour: nomes, exhibition: animais } };
+  } return { [nome]: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' } };
+}
+
+function funcDefault() {
+  const dict = {};
+  days.forEach((elemento) => {
+    const resultado = daySolo(elemento);
+    dict[elemento] = resultado[elemento];
+  });
+  return dict;
+}
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (verific(scheduleTarget) !== undefined && !days.includes(scheduleTarget)) {
+    return animationSolo(scheduleTarget);
+  } if (days.includes(scheduleTarget)) {
+    return daySolo(scheduleTarget);
+  } return funcDefault();
 }
 
 module.exports = getSchedule;
