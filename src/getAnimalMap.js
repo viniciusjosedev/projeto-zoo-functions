11e03bfc085310611a07f6a22a15a815666d78d6
Vinicius José <viniciusjosedev@gmail.com>
2022-11-03 20:01:20 -0300

    correção de erros

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,39 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+const dict = { NE: [], NW: [], SE: [], SW: [] };
+
+const order = ['NE', 'NW', 'SE', 'SW'];
+
+function funcDefault(options) {
+  order.forEach((elemento) => {
+    data.species.forEach((elemento2) => {
+      if (elemento === elemento2.location) return dict[elemento].push(elemento2.name);
+    });
+  });
+  return dict;
 }
+
+function complementDefault(lista) {
+  return lista.map((elemento3) => elemento3.name);
+}
+
+function includeTrue() {
+  order.forEach((elemento) => {
+    data.species.forEach((elemento2) => {
+      if (elemento === elemento2.location) {
+        dict[elemento].push({ [elemento2.name]: complementDefault(elemento2.residents) });
+      }
+    });
+  });
+  return dict;
+}
+
+function getAnimalMap(options = undefined) {
+  if (options === undefined || !Object.keys(options).includes('includeNames')) {
+    return funcDefault();
+  } if (Object.keys(options).includes('includeNames')) return includeTrue();
+}
+
+console.log(getAnimalMap({ includeNames: true }));
 
 module.exports = getAnimalMap;
