fb7696353d7799db936d72bb06016da1db41fc17
Vinicius José <viniciusjosedev@gmail.com>
2022-11-04 19:32:22 -0300

    requisito 14 finalizado, projeto finalizado

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,10 +1,9 @@
 const data = require('../data/zoo_data');
 
-const dict = { NE: [], NW: [], SE: [], SW: [] };
-
 const order = ['NE', 'NW', 'SE', 'SW'];
 
 function funcDefault(options) {
+  const dict = { NE: [], NW: [], SE: [], SW: [] };
   order.forEach((elemento) => {
     data.species.forEach((elemento2) => {
       if (elemento === elemento2.location) return dict[elemento].push(elemento2.name);
@@ -13,27 +12,53 @@ function funcDefault(options) {
   return dict;
 }
 
-function complementDefault(lista) {
-  return lista.map((elemento3) => elemento3.name);
+function complementDefault(lista, sexo) {
+  const lista2 = [];
+  if (sexo === 'female' || sexo === 'male') {
+    lista.forEach((elemento) => {
+      if (elemento.sex === sexo) lista2.push(elemento.name);
+    });
+    return lista2.map((elemento3) => elemento3[0].toUpperCase() + elemento3.slice(1)
+      .toLowerCase());
+  } return lista.map((elemento3) => elemento3.name[0].toUpperCase() + elemento3.name.slice(1)
+    .toLowerCase());
 }
 
-function includeTrue() {
+function verificacao(ordenado, sexo, elemento2) {
+  if (ordenado === false) {
+    return complementDefault(elemento2.residents, sexo);
+  } return complementDefault(elemento2.residents, sexo).sort();
+}
+
+function includeSoTrue(ordenado = false, sexo = false) {
+  const dict = { NE: [], NW: [], SE: [], SW: [] };
   order.forEach((elemento) => {
     data.species.forEach((elemento2) => {
       if (elemento === elemento2.location) {
-        dict[elemento].push({ [elemento2.name]: complementDefault(elemento2.residents) });
+        dict[elemento].push({ [elemento2.name]: verificacao(ordenado, sexo, elemento2) });
       }
     });
   });
   return dict;
 }
 
+function controller(options) {
+  const verific = Object.keys(options);
+  if (verific.includes('sorted') && verific.length === 2) {
+    return includeSoTrue(true);
+  } if (verific.includes('sex') && verific.length === 2) {
+    return includeSoTrue(false, options.sex);
+  } return includeSoTrue(true, options.sex);
+}
+
 function getAnimalMap(options = undefined) {
   if (options === undefined || !Object.keys(options).includes('includeNames')) {
     return funcDefault();
-  } if (Object.keys(options).includes('includeNames')) return includeTrue();
+  } if (Object.keys(options).includes('includeNames') && Object.keys(options)
+    .length === 1) return includeSoTrue();
+  return controller(options);
 }
 
-console.log(getAnimalMap({ includeNames: true }));
+console.log(getAnimalMap({ includeNames: true, sex: 'female' }));
 
 module.exports = getAnimalMap;
